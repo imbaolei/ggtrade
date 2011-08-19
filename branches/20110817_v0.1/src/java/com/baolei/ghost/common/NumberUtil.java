f367cb67604ba71c83ea060a0fee19514b9db8fa
imbaolei <imbaolei@d057189c-54fa-5b85-1262-806553943684>
2011-08-19 13:49:30 +0000

    share 稳定

--- branches/20110817_v0.1/src/java/com/baolei/ghost/common/NumberUtil.java
+++ branches/20110817_v0.1/src/java/com/baolei/ghost/common/NumberUtil.java
@@ -8,7 +8,7 @@ public class NumberUtil {
 	static DecimalFormat decimalFormat = new DecimalFormat("#.00");
 	public static float roundDown(float value,int scale){
 		BigDecimal bd = new BigDecimal(value);
-		bd.setScale(scale, BigDecimal.ROUND_DOWN);
+		bd = bd.setScale(scale, BigDecimal.ROUND_DOWN);
 		Float returnValue = bd.floatValue();
 		returnValue = Float.parseFloat(decimalFormat.format(returnValue));
 		return returnValue;
