f32b39e297f983c9d5becb1966ca21c84e7b1c6b
imbaolei <imbaolei@d057189c-54fa-5b85-1262-806553943684>
2011-08-20 14:08:26 +0000

    add firstday of month

--- branches/20110817_v0.1/src/java/com/baolei/trade/test/Test.java
+++ branches/20110817_v0.1/src/java/com/baolei/trade/test/Test.java
@@ -30,7 +30,7 @@ public abstract class Test {
 	protected Map<String, StockDO> pdStockMap;
 	protected Map<String, StockDO> jyStockMap;
 	protected float moneyDingTou; // 每个周期定投的数额
-	protected float rateHR = 0.008f;
+	protected float rateHR = 0.0135f;
 	protected float cash; // LowRisk低风险头寸
 	protected float toucunHR; // HighRisk高风险头寸
 	protected float shareHR; // HighRisk高风险份额
